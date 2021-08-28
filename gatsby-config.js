6132fa81a667bf06e77bc0abf4ac37be164c82fc
Sebi <SebastianStoff@gmx.at>
2021-08-28 19:57:59 +0200

    #1. upadted config to use ts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Sebastian Stoff portfolio",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      }
+    }
+  ]
 };
