ce4897c3b487978ac4e39a4f0f117cff8a27b39f
Sebi <SebastianStoff@gmx.at>
2021-08-31 09:06:19 +0200

    #7. Added template component

---
+++ src/templates/OrcidPerson.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pokemon = (props) => {
+  console.log(props);
+  return (
+    <div style={{ width: 960, margin: "4rem auto" }}>
+      <h1>Orcid page</h1>
+    </div>
+  );
+};
+
+export default Pokemon;
