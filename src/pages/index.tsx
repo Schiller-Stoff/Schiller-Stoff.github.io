82abbff0084d91d1b5278ad56cb40d028135908a
Sebastian Stoff <sebi.stoff@gmail.com>
2021-09-08 15:13:35 +0200

    #14. using now own orcid - icon bar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import BaseLayout from "../components/shared/layouts/BaseLayout";
+import OrcidIconBar from "../components/shared/OrcidIconBar";
 import OrcidTimeline from "../components/shared/OrcidTimeline";
 import Timeline from "../components/shared/Timeline";
 import StyleOverlay from "../components/static/EntryHero";
@@ -106,20 +107,11 @@ const IndexPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> = (
           </Col>
         </Row>
         <br />
-        <div>
-        <a href={props.pageContext.persOrcid["orcid-identifier"].uri} target="_blank"><img width={48} height={40} src="https://theme.zdassets.com/theme_assets/2284388/5f241602bd45df20fe02a537477dc62c1e1ed582.png" style={{ paddingLeft: ".5em" }}></img></a>
-        {props.pageContext.persOrcid.person["researcher-urls"][
-          "researcher-url"
-        ].map((rurl) => {
-          const imgWidth = 50;
-          const imgHeight = 50;
-          if (rurl.url.value.toString().includes("github")) {
-            return <a href={rurl.url.value} target="_blank"><img width={imgWidth} height={imgHeight} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></img></a>
-          } else if(rurl.url.value.toString().includes("npmjs.com")){
-            return <a href={rurl.url.value} target="_blank"><img width={imgWidth} height={23} src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/2560px-Npm-logo.svg.png"></img></a>
-          }
-        })}
-        </div>
+        <OrcidIconBar 
+          orcidURL={props.pageContext.persOrcid["orcid-identifier"].uri} 
+          researchURLs={props.pageContext.persOrcid.person["researcher-urls"]["researcher-url"]} 
+        />
+
 
         <br />
         <br />
