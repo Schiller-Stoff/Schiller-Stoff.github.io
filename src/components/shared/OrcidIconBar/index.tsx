7605b34a92f91cf4201066fb0199525cd1c06e25
Sebastian Stoff <sebi.stoff@gmail.com>
2021-09-08 16:00:08 +0200

    #14. added jsdoc and removed unnecessary code parts

--- src/components/shared/OrcidIconBar/index.tsx
+++ src/components/shared/OrcidIconBar/index.tsx
@@ -11,6 +11,11 @@ interface Props {
 }
 
 
+/**
+ * Handles display of orcid research urls as icons
+ * @param props 
+ * @returns 
+ */
 const OrcidIconBar: React.FC<Props> = (props) => {
 
   const faIconStyle: React.CSSProperties =  {
@@ -20,17 +25,12 @@ const OrcidIconBar: React.FC<Props> = (props) => {
     display:"inline-block"
   }
 
-  console.log(props)
-  debugger;
-
   return (
     <div>
       {props.mail && <a style={faIconStyle} href={`mailTo:${props.mail}`}><FontAwesomeIcon icon={faEnvelope} size="2x"></FontAwesomeIcon></a>}
     
       <a style={faIconStyle} href={props.orcidURL} target="_blank"><FontAwesomeIcon icon={faOrcid} size="2x"></FontAwesomeIcon></a>
       {props.researchURLs.map((rurl) => {
-        const imgWidth = 50;
-        const imgHeight = 50;
         if (rurl.url.value.toString().includes("github")) {
           return <a style={faIconStyle} href={rurl.url.value} target="_blank"><FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon></a>
         } else if (rurl.url.value.toString().includes("npmjs.com")) {
