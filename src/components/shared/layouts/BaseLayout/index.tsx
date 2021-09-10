753c516ac51250b3667098573634960ec0698393
Sebastian Stoff <sebi.stoff@gmail.com>
2021-09-10 12:44:56 +0200

    #19. Added react helmet with meta to baselayout

--- src/components/shared/layouts/BaseLayout/index.tsx
+++ src/components/shared/layouts/BaseLayout/index.tsx
@@ -5,9 +5,11 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../../../styles/base.css';
 import Sidebar from "../../../static/Sidebar";
+import Head from "../../Head/indext";
 
 const BaseLayout: React.FC = (props) => (
   <>
+    <Head></Head>
     <MyNav></MyNav>
     <Sidebar></Sidebar>
     <Container className="min-vh-100 mt-md-2 pt-5 px-md-3" as="main"><br/>{props.children}</Container>
