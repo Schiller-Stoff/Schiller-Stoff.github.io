9a10912ad72e135bda367a84b1963f944d2a5f4a
Sebi <SebastianStoff@gmx.at>
2021-09-11 16:11:38 +0200

    #19. added missing alt attributes from sidebar images

--- src/components/static/Sidebar/index.tsx
+++ src/components/static/Sidebar/index.tsx
@@ -15,11 +15,11 @@ const Sidebar: React.FC = () => {
       className="d-lg-block d-none"
       style={asideStyle}>
       <a target="_blank" href="https://informationsmodellierung.uni-graz.at/en/">
-        <img width={25} src="https://static.uni-graz.at/fileadmin/_processed_/1/3/csm_2018-11-22_ZIM_schwarz_typo3_24124f9364.png"></img>
+        <img alt="ZIM Graz logo" width={25} src="https://static.uni-graz.at/fileadmin/_processed_/1/3/csm_2018-11-22_ZIM_schwarz_typo3_24124f9364.png"></img>
       </a>
       <br/>
       <a target="_blank" href="https://www.uni-graz.at/en/">
-        <img style={{border: "1px solid lightgrey"}} width={25} src="https://static.uni-graz.at/fileadmin/grafik/logos_vorschau/logo_uni_graz_sw_ohne.jpg"></img>
+        <img alt="University of Graz logo" style={{border: "1px solid lightgrey"}} width={25} src="https://static.uni-graz.at/fileadmin/grafik/logos_vorschau/logo_uni_graz_sw_ohne.jpg"></img>
       </a>
       <br/>
 
