d3dfbdc2509526ee32309e11b60029b3cbf2e751
Sebi <SebastianStoff@gmx.at>
2021-09-12 18:37:41 +0200

    #26. nav links on top now rendered according to returned orcid data

--- src/components/shared/OrcidNav/index.tsx
+++ src/components/shared/OrcidNav/index.tsx
@@ -8,22 +8,27 @@ interface Props {
 }
 
 /**
- *
+ * Nav rendered according to given ORCID-APIv2 root object.
  */
 const OrcidNav: React.FC<Props> = (props) => {
+  /**
+   * Prefers the first alsoKnownAs name given to ORCID. Falls back to main name if not provided.
+   * @param root orcid root object
+   * @returns name extracted of orcid data as string
+   */
   const buildName = (root: Orcid.RootObject) => {
     // prefer check if also known as is defined -> prefered label to show
     let firstAlsoKnownAs = MyStringUtils.catchToString(
       () => root.person["other-names"]["other-name"][0].content
     );
-    if(firstAlsoKnownAs) return firstAlsoKnownAs;
+    if (firstAlsoKnownAs) return firstAlsoKnownAs;
 
     // else use first + familenames
     let name = "";
 
     // add firstnames.
-    name += MyStringUtils.catchToString(() =>
-      root.person["other-names"]["other-name"][0].content
+    name += MyStringUtils.catchToString(
+      () => root.person["other-names"]["other-name"][0].content
     );
 
     //add last name.
@@ -34,7 +39,53 @@ const OrcidNav: React.FC<Props> = (props) => {
     return name;
   };
 
-  return <MyNav title={buildName(props.orcidRoot)} />;
+  const buildNavSideLinks = (root: Orcid.RootObject) => {
+    let sideLinks: { label: string; href: string }[] = [];
+
+    if (root["activities-summary"].works.group[0]) {
+      sideLinks.push({
+        label: "Works",
+        href: "#works",
+      });
+    }
+
+    if (root["activities-summary"].educations["education-summary"][0]) {
+      sideLinks.push({
+        label: "Educations",
+        href: "#educations",
+      });
+    }
+
+    if (root["activities-summary"].employments["employment-summary"][0]) {
+      sideLinks.push({
+        label: "Employments",
+        href: "#employments",
+      });
+    }
+
+    if (root["activities-summary"].fundings.group[0]) {
+      sideLinks.push({
+        label: "Fundings",
+        href: "#fundings",
+      });
+    }
+
+    if (root["activities-summary"]["peer-reviews"].group[0]) {
+      sideLinks.push({
+        label: "Peer Reviews",
+        href: "#peerreviews",
+      });
+    }
+
+    return sideLinks.length > 1 ? sideLinks : null;
+  };
+
+  return (
+    <MyNav
+      title={buildName(props.orcidRoot)}
+      sideLinks={buildNavSideLinks(props.orcidRoot)}
+    />
+  );
 };
 
 export default OrcidNav;
--- src/components/static/Nav/index.tsx
+++ src/components/static/Nav/index.tsx
@@ -4,48 +4,75 @@ import { Link } from "gatsby";
 
 interface Props {
   title: string;
+  sideLinks?: {
+    label: string;
+    href: string;
+  }[];
 }
 
 const MyNav: React.FC<Props> = (props) => {
-
   const navStyle: React.CSSProperties = {
     fontSize: "1em",
     fontWeight: 600,
-    color: "grey"
-  }
+    color: "grey",
+  };
 
   return (
-      <Navbar collapseOnSelect expand="lg">
-        <Container>
+    <Navbar collapseOnSelect expand="lg">
+      <Container>
         <Nav style={navStyle}>
           <Nav.Link as="li">
             <Link to="/" className="text-dark text-decoration-none">
               {props.title}
             </Link>
           </Nav.Link>
+        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end px-4"
+        >
+          <Nav style={navStyle}>
+            {props.sideLinks ? (
+              props.sideLinks.map((linkObj, index) => (
+                <Nav.Link key={linkObj.href + index} as="li">
+                  <Link
+                    to={linkObj.href}
+                    className="text-dark text-decoration-none"
+                  >
+                    {linkObj.label}
+                  </Link>
+                </Nav.Link>
+              ))
+            ) : (
+              <>
+                <Nav.Link as="li">
+                  <Link to="/about" className="text-dark text-decoration-none">
+                    About
+                  </Link>
+                </Nav.Link>
+                <Nav.Link href="#features" as="li">
+                  <Link
+                    to="/projects"
+                    className="text-dark text-decoration-none"
+                  >
+                    Projects
+                  </Link>
+                </Nav.Link>
+                <Nav.Link href="#features" as="li">
+                  <Link
+                    to="/contact"
+                    className="text-dark text-decoration-none"
+                  >
+                    Contact
+                  </Link>
+                </Nav.Link>
+              </>
+            )}
           </Nav>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse
-            id="responsive-navbar-nav"
-            className="justify-content-end px-4"
-          >
-            <Nav style={navStyle}>
-              <Nav.Link as="li">
-                <Link to="/about" className="text-dark text-decoration-none">About</Link>
-              </Nav.Link>
-              <Nav.Link
-                href="#features"
-                as="li"
-              >
-                <Link to="/projects" className="text-dark text-decoration-none">Projects</Link>
-              </Nav.Link>
-              <Nav.Link href="#features" as="li">
-                <Link to="/contact" className="text-dark text-decoration-none">Contact</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
