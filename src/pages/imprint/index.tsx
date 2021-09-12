69dac86d644ec5c0c13af3e33dcd9f9986fd5492
Sebi <SebastianStoff@gmx.at>
2021-09-12 13:08:21 +0200

    #20. added imprint with some acknowledgements

--- src/pages/imprint/index.tsx
+++ src/pages/imprint/index.tsx
@@ -8,10 +8,16 @@ import BaseLayout from "../../components/shared/layouts/BaseLayout";
 const Imprint: React.FC = () => {
   return (
     <BaseLayout>
+      <br/>
+      <br/>
       <h1>Imprint</h1>
-      <h2>Acknoledgments</h2>
-      <h3>Apple Touch icons</h3>
+      <br/>
+      <h2 className="h3">Acknowledgments</h2>
+      <h3 className="h4">Apple Touch icons</h3>
       <p>by "Apple icon by <a href="https://icons8.com/" target="_blank">Icons8</a>"</p>
+      <h3>Themes and icons</h3>
+      <p><a href="https://gatsby-theme-portfolio.netlify.app/" target="_blank">Smakosh gatsby theme</a></p>
+      <p><a href="https://fontawesome.com/" target="_blank">Fontawesome</a></p>
     </BaseLayout>
   );
 };
