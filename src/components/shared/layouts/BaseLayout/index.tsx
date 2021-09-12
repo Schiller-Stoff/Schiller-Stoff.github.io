27f6c8abe9f020d27059cb15f521655fb2178193
Sebi <SebastianStoff@gmx.at>
2021-09-12 17:42:11 +0200

    #25. BaseLayout now also accepts a custom nav component

--- src/components/shared/layouts/BaseLayout/index.tsx
+++ src/components/shared/layouts/BaseLayout/index.tsx
@@ -2,21 +2,29 @@ import { Container } from "react-bootstrap";
 import Footer from "../../../static/Footer";
 import MyNav from "../../../static/Nav";
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../../../styles/base.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../../../styles/base.css";
 import Sidebar from "../../../static/Sidebar";
 import Head from "../../Head";
 
 interface Props {
-  headComponent?: JSX.Element
+  headComponent?: JSX.Element;
+  nav?: JSX.Element;
 }
 
+/**
+ * BaseLayout component of page
+ * @param props
+ * @returns
+ */
 const BaseLayout: React.FC<Props> = (props) => (
   <>
-    {props.headComponent ? props.headComponent : <Head/>}
-    <MyNav></MyNav>
+    {props.headComponent ? props.headComponent : <Head />}
+    {props.nav ? props.nav : <MyNav />}
     <Sidebar></Sidebar>
-    <Container className="min-vh-100 px-md-3" as="main"><br/>{props.children}</Container>
+    <Container className="min-vh-100 px-md-3" as="main">
+      {props.children}
+    </Container>
     <Footer></Footer>
   </>
 );
