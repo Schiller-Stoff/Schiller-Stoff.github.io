3a4b187445f53a5c8c58ff1b0960bf6216bf736a
Sebastian Stoff <sebi.stoff@gmail.com>
2021-09-14 13:28:49 +0200

    #20. Added content to the imrpint page

--- src/pages/imprint/index.tsx
+++ src/pages/imprint/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import BaseLayout from "../../components/shared/layouts/BaseLayout";
+import { Orcid } from "../../types/orcid";
 
 /**
  * Imprint page component
  * @returns 
  */
-const Imprint: React.FC = () => {
+const Imprint: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> = (props) => {
+  console.log(props.pageContext.persOrcid);
   return (
     <BaseLayout>
       <br/>
       <br/>
-      <h1>Imprint</h1>
+      <h1>Imprint and Data protection</h1>
       <br/>
-      <h2>Data protection</h2>
+      <h2>Publisher, development and implementation</h2>
+      <p>Personal project. (Legal responsibilities according to CV information on main page)</p>
+      <h3>The following technologies form an integral part of the website</h3>
+      <p>Gatsby.js, React.js and Typescript </p>
+      <h3>Information according to § 5 E-Commerce Law</h3>
+      <p>All content and services on this website are provided by the owner of the website stated on home-page</p>
       <br/>
-      <h2 className="h3">Acknowledgments</h2>
+      <h2>Acknowledgments</h2>
+      <br />
       <h3 className="h4">Apple Touch icons</h3>
       <p>by "Apple icon by <a href="https://icons8.com/" target="_blank">Icons8</a>"</p>
       <h3>Themes and icons</h3>
