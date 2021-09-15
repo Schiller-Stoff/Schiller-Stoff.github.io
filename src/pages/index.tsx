1d85904728a055d4b60bb3a17dd357f0441a9f4d
Sebi <SebastianStoff@gmx.at>
2021-09-15 14:16:19 +0200

    #37. added alt attribute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ const IndexPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> = (
               <Col md={12} lg={10} xl={8}>
                 <OrcidBiography orcidRoot={props.pageContext.persOrcid} />
                 <br />
-                <img src="/illustrations/dev.svg" className="w-50" loading="lazy"></img>
+                <img alt="Programmer sitting on the floor" src="/illustrations/dev.svg" className="w-50" loading="lazy"></img>
                 <br />
                 <br />
                 <br />
