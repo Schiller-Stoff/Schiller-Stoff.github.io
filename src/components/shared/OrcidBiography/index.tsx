7fd8a0a37cced54d6466a272d207c54f16657eca
Sebi <SebastianStoff@gmx.at>
2021-09-27 11:02:08 +0200

    #35. added bio length switch

--- src/components/shared/OrcidBiography/index.tsx
+++ src/components/shared/OrcidBiography/index.tsx
@@ -9,12 +9,17 @@ interface Props {
 
 const OrcidBiography: React.FC<Props> = (props) => {
 
+  const [bioLength, setBioLength] = React.useState<"shortest" | "short" | "medium" | "full">("shortest");
+
+
   /**
    * Adds breaks between different sentences in given text via ORCID.
    * @param bio 
    * @returns 
    */
-  const increaseReadability = (bio: string) => {
+  const increaseReadability = (bio: string, wishedLength: "shortest" | "short" | "medium" | "full") => {
+    bio = adaptLength(bio, wishedLength);
+
     const split = bio.split(".");
     if(split.length < 3)return <p>{bio}</p>;
 
@@ -36,6 +41,32 @@ const OrcidBiography: React.FC<Props> = (props) => {
   }
 
   /**
+   * Reduces length of given text according to "." dots in the text. Reductions is done
+   * according to given second parameter.
+   * @param bio text to reduce in sentences.
+   * @param wishedLength length spec defined as enums.
+   * @returns reduced text
+   */
+  const adaptLength = (bio: string, wishedLength: "shortest" | "short" | "medium" | "full") => {
+
+    let bioSplit = bio.split(".");
+
+    switch(wishedLength){
+      case "shortest":
+        return bioSplit[0] + "."
+      case "short":
+        return `${bioSplit[0]}.${bioSplit[1]}.`
+      case "medium":
+        return `${bioSplit[0]}.${bioSplit[1]}.${bioSplit[2]}.${bioSplit[3]}.`
+      case "full":
+        return bio;
+      default:
+        return bio;
+    }
+
+  }
+
+  /**
    * Renders content conditionally according to given orcid API response.
    * @param root orcidRoot object
    * @returns either valid JSX or null when no biography was given.
@@ -47,12 +78,22 @@ const OrcidBiography: React.FC<Props> = (props) => {
       return null
     } else {
       return (<div id="biography" className={props.className}>
-        <div className="">{ increaseReadability(bioOrcid)}</div>
+        <div className="">{ increaseReadability(bioOrcid, bioLength)}</div>
       </div>)
     };
   }
 
-  return renderConditional(props.orcidRoot);
+  return (
+  <> 
+    <div>
+      <input className="ms-1" checked={bioLength === "shortest"} type="radio" id="orcid_bio_radio1" name="fav_language" onClick={() => setBioLength("shortest")}></input>
+      <input className="ms-1" type="radio" id="orcid_bio_radio2" name="fav_language" onClick={() => setBioLength("short")}></input>
+      <input className="ms-1" type="radio" id="orcid_bio_radio3" name="fav_language" onClick={() => setBioLength("medium")}></input>
+      <input className="ms-1" type="radio" id="orcid_bio_radio4" name="fav_language" onClick={() => setBioLength("full")}></input>
+      <small style={{color: "lightgrey"}}> | Choose length</small>
+    </div>
+    {renderConditional(props.orcidRoot)}
+  </>)
 
 }
 
