69837254bc0e1d7b7d8d2cfe62005d00609e5524
Sebi <SebastianStoff@gmx.at>
2021-09-30 19:28:03 +0200

    #38. updated markdown remark styling

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,34 +1,39 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 import BaseLayout from "../components/shared/layouts/BaseLayout";
+import { Col, Row } from "react-bootstrap";
 
 export default function Template({
   pageContext,
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
   return (
-    <BaseLayout
-      orcidRoot={pageContext.persOrcid}
-    >
-    <div className="blog-post-container" style={{minHeight:"75vh"}}>
-      <div className="blog-post">
-        <p><strong>{frontmatter.title}</strong> | {frontmatter.date}</p>
-        <hr/>
-        <br/>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <BaseLayout orcidRoot={pageContext.persOrcid}>
+      <div className="blog-post-container" style={{ minHeight: "75vh" }}>
+        <div className="blog-post">
+          <p>
+            <strong>{frontmatter.title}</strong> | {frontmatter.date}
+          </p>
+          <hr />
+          <br />
+          <Row>
+            <Col lg={8}>
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
     </BaseLayout>
-  )
+  );
 }
 
 export const pageQuery = graphql`
-  query($id: String!) {
+  query ($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
@@ -38,4 +43,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
