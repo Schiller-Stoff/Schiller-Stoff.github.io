881ea892fa9c66a2b22a87fa79f244f64301b541
Sebi <SebastianStoff@gmx.at>
2021-09-30 10:08:29 +0200

    #38. Corrected blog page rendering a bit

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,22 +1,30 @@
 import React from "react"
 import { graphql } from "gatsby"
+import BaseLayout from "../components/shared/layouts/BaseLayout";
+import OrcidNav from "../components/shared/OrcidNav";
 
 export default function Template({
+  pageContext,
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
+    <BaseLayout
+      orcidRoot={pageContext.persOrcid}
+    >
     <div className="blog-post-container">
       <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <p><strong>{frontmatter.title}</strong> | {frontmatter.date}</p>
+        <hr/>
+        <br/>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
     </div>
+    </BaseLayout>
   )
 }
 
