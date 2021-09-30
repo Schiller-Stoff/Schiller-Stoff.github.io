fe8d468af8c23bb67c03531a7f033dafcbd52022
Sebi <SebastianStoff@gmx.at>
2021-09-30 09:51:44 +0200

    #38. Added mardkown processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,14 @@ module.exports = {
         allExtensions: true, // defaults to false
       }
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`
   ]
 };
