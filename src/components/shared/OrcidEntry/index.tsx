b81ab785cae02a31c297fd51d82d56870a75ad48
Sebi <SebastianStoff@gmx.at>
2022-02-18 19:57:30 +0100

    #39. updated design

--- src/components/shared/OrcidEntry/index.tsx
+++ src/components/shared/OrcidEntry/index.tsx
@@ -6,6 +6,7 @@ import { Col, Row, Button } from "react-bootstrap";
 import { propTypes } from "react-bootstrap/esm/Image";
 import { Orcid } from "../../../types/orcid";
 import OrcidBiography from "../OrcidBiography";
+import Typed from "react-typed";
 
 interface Props {
   orcidRoot: Orcid.RootObject;
@@ -17,15 +18,41 @@ interface Props {
 const OrcidEntry: React.FC<Props> = ({ orcidRoot }) => {
   return (
     <section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Row>
         <Col md={12} xl={10}>
-          <h1 className="m-0" id="start" style={{ fontSize: "3em" }}>
-            
-              {orcidRoot.person.name["given-names"].value}{" "}
-              {orcidRoot.person.name["family-name"].value}
-            
+          <p style={{marginBottom: "-.75em", fontSize:".75em"}}>DEV</p>
+          <p style={{marginBottom: "-.75em", fontSize:".75em"}}>RESEARCH</p>
+          <p style={{marginBottom: "-.75em", fontSize:".75em"}}>SOFTWARE SOLUTIONS</p>
+          <h1 className="m-0" id="start" style={{ fontSize: "4em" }}>
+            {orcidRoot.person.name["given-names"].value}{" "}
+            {orcidRoot.person.name["family-name"].value}
           </h1>
-          <h2
+
+          <span style={{fontSize:"1.5em"}}>... </span> <Typed
+            loop
+            strings={[
+              "does #DigitalHumanities",
+              "loves #SoftwareDevelopment ",
+              "cares about #SoftwareArchitecture",
+              "worries about #SoftwareQuality"
+              //"writes his dissertation about the role #SoftwareArchitecture and #SoftwareDesign have to play in the field of #DigitalHumanities to ensure #SoftwareQuality in scientific research."
+            ]}
+            typeSpeed={10}
+            backSpeed={20}
+            backDelay={1000}
+            style={{ fontSize: "1.5em"}}
+            showCursor={true}
+            className="self-typed"
+            cursorChar="|"
+          />
+          {/* <h2
             className="text-dark h5 mb-4"
             style={{ fontWeight: 300, fontSize: "1.35em" }}
           >
@@ -40,19 +67,19 @@ const OrcidEntry: React.FC<Props> = ({ orcidRoot }) => {
                 "employment-summary"
               ][0].organization.name
             }
-          </h2>
+          </h2> */}
           {/* <hr></hr> */}
-          <OrcidBiography orcidRoot={orcidRoot}></OrcidBiography>
-          <Link className="btn btn-light" to="/portfolio">
+          {/* <OrcidBiography orcidRoot={orcidRoot}></OrcidBiography> */}
+          {/* <Link className="btn btn-light" to="/portfolio">
             <FontAwesomeIcon icon={faTh} size="1x" /> View portfolio
           </Link>{" "}
           <Link className="btn btn-light" to="/#employments">
             <FontAwesomeIcon icon={faAlignJustify} size="1x" /> View resume
-          </Link>
+          </Link> */}
         </Col>
         <Col>
           <div className="mt-md-4 m-0">
-            <img
+            {/* <img
               alt="Main profile picture"
               src={
                 process.env.LINK_TO_PROFILE_IMG
@@ -62,11 +89,27 @@ const OrcidEntry: React.FC<Props> = ({ orcidRoot }) => {
               width={150}
               height={150}
               style={{
-                borderRadius: "100%",
+                // borderRadius: "100%",
                 margin: "1em 0",
                 filter: "grayscale(150%)",
               }}
-            ></img>
+            ></img> */}
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
           </div>
         </Col>
       </Row>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,11 +23,6 @@ const IndexPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> = (
         headComponent={<OrcidHead orcidRoot={props.pageContext.persOrcid} />}
         nav={<OrcidNav orcidRoot={props.pageContext.persOrcid} />}
       >
-        {
-          // temporarily set display none to tabs view!
-        }
-        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" style={{ display: "none" }} className="mb-3 w-50">
-          <Tab eventKey="home" title="Academic Style">
             <OrcidEntry orcidRoot={props.pageContext.persOrcid}></OrcidEntry>
             {/* <br /> */}
             <br />
@@ -38,92 +33,12 @@ const IndexPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> = (
             <br />
             <hr />
             <br />
-            <Row>
-              <Col md={12} lg={10} xl={10}>
-                {/* <img height={300} width={400} alt="Programmer sitting on the floor" src="/illustrations/dev.svg" className="w-50" loading="lazy"></img> */}
-                <br />
-                <h2 className="h3" id="employments">
-                  Current and past employments
-                </h2>
-                <div>
-
-                  <p>
-                    Defined by the ORCID ontology: Employment is a formal employment
-                    relationship with an organization, e.g. staff, intern,
-                    researcher, contractor. Employment can be paid or unpaid.
-                  </p>
-                </div>
-                <div style={{ maxWidth: "900px" }}>
-                  <OrcidTimeline
-                    orcidSequence={
-                      props.pageContext.persOrcid["activities-summary"].employments
-                    }
-                    mode="VERTICAL_ALTERNATING"
-                  ></OrcidTimeline>
-                </div>
-                <br />
-                <br />
-                <br />
-
-                {props.pageContext.persOrcid["activities-summary"].educations[
-                  "last-modified-date"
-                ] && (
-                    <div className="px-3">
-                      <h2 className="h3" id="educations">
-                        Education and qualifications
-                      </h2>
-                      <p>
-                        Education is participation in an academic higher education
-                        program to receive an undergraduate, graduate, or other
-                        degree. Qualification is participation in a professional or
-                        vocational accreditation, certification, or training program.
-                        Both may be in progress or unfinished.
-                      </p>
-                      <div style={{ maxWidth: "900px" }}>
-                        <OrcidTimeline
-                          mode="VERTICAL_ALTERNATING"
-                          orcidSequence={
-                            props.pageContext.persOrcid["activities-summary"].educations
-                          }
-                        ></OrcidTimeline>
-                      </div>
-                      <br></br>
-                      <br></br>
-                      <br></br>
-                    </div>
-                  )}
-                <h2 className="h3" id="works">
-                  Works: Things I have accomplished
-                </h2>
-                <p>
-                  ORCID defines works as your research outputs, including
-                  publications, data sets, conference presentations, and more.
-                </p>
-                <Container>
-                  <div style={{ maxWidth: "900px" }}>
-                    <OrcidTimeline
-                      orcidSequence={
-                        props.pageContext.persOrcid["activities-summary"].works
-                      }
-                      mode="VERTICAL_ALTERNATING"
-                      CardComp={OrcidWorkCard}
-                    />
-                  </div>
-                </Container>
-              </Col>
-              <Col></Col>
-            </Row>
+            
             <br></br>
             <br></br>
             <br></br>
-          </Tab>
-          <Tab eventKey="profile" title="Developer Style">
-            <p>Test</p>
-          </Tab>
-          <Tab eventKey="contact" title="Conventional Style">
-            <p>Test</p>
-          </Tab>
-        </Tabs>
+          
+        
 
 
       </BaseLayout>
