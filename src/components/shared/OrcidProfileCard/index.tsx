c0dc5092608d4f70016bf1b5196727db4eb533b0
Sebi <SebastianStoff@gmx.at>
2022-02-20 10:40:40 +0100

    #39. reformat

--- src/components/shared/OrcidProfileCard/index.tsx
+++ src/components/shared/OrcidProfileCard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, Col, Accordion, Button } from "react-bootstrap";
 import { Orcid } from "../../../types/orcid";
+import ContentHeading from "../ContentHeading";
 
 interface Props {
   orcidRoot: Orcid.RootObject;
@@ -9,86 +10,91 @@ interface Props {
 const OrcidProfileCard: React.FC<Props> = ({ orcidRoot }) => {
   return (
     <>
-    <img
-              alt="Main profile picture"
-              src={
-                process.env.LINK_TO_PROFILE_IMG
-                  ? process.env.LINK_TO_PROFILE_IMG
-                  : "/img/profile.jpg"
-              }
-              width={250}
-              height={250}
-              style={{
-                borderRadius: "2%",
-                margin: "1em 0",
-                filter: "grayscale(150%)",
-              }}
-            ></img>
-    <Row>
-      <Col lg={6}>
-        {/* <Button variant="secondary">Request meeting</Button> */}
-        <div>
-          
-              <h3 className="h5">Interests &amp; skills</h3>
-              {orcidRoot.person.keywords.keyword
-                      .map((kword, index) => (
-                        <Button className="m-1" variant="outline-dark" key={`kword_${index}`}>{" " + kword.content}</Button>
-                      ))}
-
-        </div>
-      </Col>
-      <Col md={5} xl={6}>
-        <br className="d-lg-none"/>
-        <h3 className="h5 invisible">Active networks &amp; working groups</h3>
-        <Accordion defaultActiveKey="0" flush className="border">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>My primary background</Accordion.Header>
-            <Accordion.Body>
-              {orcidRoot.person.keywords.keyword[0] && (
-                <>
-                  <ul>
-                    {orcidRoot.person.keywords.keyword
-                      .slice(0, 5)
-                      .map((kword, index) => (
-                        <li key={`kword_${index}`}>
-                          <small>{" " + kword.content}</small>
+      <ContentHeading>Whoami?</ContentHeading>
+      <br></br>
+      <img
+        alt="Main profile picture"
+        src={
+          process.env.LINK_TO_PROFILE_IMG
+            ? process.env.LINK_TO_PROFILE_IMG
+            : "/img/profile.jpg"
+        }
+        width={250}
+        height={250}
+        style={{
+          borderRadius: "2%",
+          margin: "1em 0",
+          filter: "grayscale(150%)",
+        }}
+      ></img>
+      <Row>
+        <Col lg={6}>
+          {/* <Button variant="secondary">Request meeting</Button> */}
+          <div>
+            <h3 className="h5">Interests &amp; skills</h3>
+            {orcidRoot.person.keywords.keyword.map((kword, index) => (
+              <Button
+                className="m-1"
+                variant="outline-dark"
+                key={`kword_${index}`}
+              >
+                {" " + kword.content}
+              </Button>
+            ))}
+          </div>
+        </Col>
+        <Col md={5} xl={6}>
+          <br className="d-lg-none" />
+          <h3 className="h5 invisible">Active networks &amp; working groups</h3>
+          <Accordion defaultActiveKey="0" flush className="border">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>My primary background</Accordion.Header>
+              <Accordion.Body>
+                {orcidRoot.person.keywords.keyword[0] && (
+                  <>
+                    <ul>
+                      {orcidRoot.person.keywords.keyword
+                        .slice(0, 5)
+                        .map((kword, index) => (
+                          <li key={`kword_${index}`}>
+                            <small>{" " + kword.content}</small>
+                          </li>
+                        ))}
+                    </ul>
+                  </>
+                )}
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>My Links</Accordion.Header>
+              <Accordion.Body>
+                {orcidRoot.person["researcher-urls"]["researcher-url"][0] && (
+                  <>
+                    <ul>
+                      {orcidRoot.person["researcher-urls"][
+                        "researcher-url"
+                      ].map((rurl, index) => (
+                        <li key={`url_${index}`}>
+                          <small>
+                            <a
+                              rel="noopener"
+                              target="_blank"
+                              className="text-dark"
+                              href={rurl.url.value.toString()}
+                            >
+                              {rurl["url-name"] ? rurl["url-name"] : "Link"}
+                            </a>
+                          </small>
                         </li>
                       ))}
-                  </ul>
-                </>
-              )}
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>My Links</Accordion.Header>
-            <Accordion.Body>
-            {orcidRoot.person["researcher-urls"]["researcher-url"][0] && (
-          <>
-            <ul>
-              {orcidRoot.person["researcher-urls"]["researcher-url"]
-                .map((rurl, index) => (
-                  <li key={`url_${index}`}>
-                    <small>
-                      <a
-                        rel="noopener"
-                        target="_blank"
-                        className="text-dark"
-                        href={rurl.url.value.toString()}
-                      >
-                        {rurl["url-name"] ? rurl["url-name"] : "Link"}
-                      </a>
-                    </small>
-                  </li>
-                ))}
-            </ul>
-          </>
-        )}
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-
-      </Col>
-    </Row>
+                    </ul>
+                  </>
+                )}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+      </Row>
     </>
   );
 };
