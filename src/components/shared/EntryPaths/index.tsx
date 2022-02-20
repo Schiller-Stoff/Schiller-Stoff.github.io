834d8f69dc905088c3ab250c39356fe7c288c295
Sebi <SebastianStoff@gmx.at>
2022-02-20 18:40:01 +0100

    #39. removed link style

--- src/components/shared/EntryPaths/index.tsx
+++ src/components/shared/EntryPaths/index.tsx
@@ -3,24 +3,21 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import ContentHeading from "../ContentHeading";
 
-
 const EntryPaths = () => {
-
   const cards = [
     {
       title: "Portfolio",
-      to:"/portfolio/"
+      to: "/portfolio/",
     },
     {
       title: "Biography",
-      to:"/bio/"
+      to: "/bio/",
     },
     {
       title: "Contact",
-      to:"/contact/"
-    }
-  ]
-
+      to: "/contact/",
+    },
+  ];
 
   return (
     <>
@@ -28,30 +25,23 @@ const EntryPaths = () => {
       <Row xs={1} md={3} className="g-4 mt-1">
         {cards.map((card, idx) => (
           <Col>
-            <Link to={card.to}>
-            <Card bg="white" className="border-0 rounded-0 border-dark">
-              {/* <Card.Img variant="top" src={
-                process.env.LINK_TO_PROFILE_IMG
-                  ? process.env.LINK_TO_PROFILE_IMG
-                  : "/img/profile.jpg"
-              } /> */}
-              <Card.Body>
-                <Card.Title>{card.title}</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <Link to={card.to} className="text-decoration-none text-dark">
+              <Card bg="white" className="border-0 rounded-0 border-dark">
+                <Card.Body>
+                  <Card.Title>{card.title}</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
             </Link>
           </Col>
         ))}
       </Row>
     </>
   );
-
-}
-
+};
 
 export default EntryPaths;
