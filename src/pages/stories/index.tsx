de007f8dee41e361f7f4b63a33ebf632755003bf
Sebi <SebastianStoff@gmx.at>
2022-02-22 14:09:25 +0100

    #40. added test component for react scrollama

--- src/pages/stories/index.tsx
+++ src/pages/stories/index.tsx
@@ -4,8 +4,18 @@ import OrcidHead from "../../components/shared/OrcidHead";
 import OrcidNav from "../../components/shared/OrcidNav";
 import { Orcid } from "../../types/orcid";
 
+/**
+ * React scrollama needs to be lazy loaded. https://www.gatsbyjs.com/docs/using-client-side-only-packages/#workaround-3-load-client-side-dependent-components-with-loadable-components
+ * Because of client only calculations that breaks SSR.
+ */
+const ClientSideOnlyLazy = React.lazy(
+  () => import("../../components/static/ScrollamaTest")
+);
+
 const StoriesPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> =
   (props) => {
+    const isSSR = typeof window === "undefined";
+
     return (
       <BaseLayout
         orcidRoot={props.pageContext.persOrcid}
@@ -13,6 +23,11 @@ const StoriesPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> =
         nav={<OrcidNav orcidRoot={props.pageContext.persOrcid} />}
       >
         <h1>Data stories demo page</h1>
+        {!isSSR && (
+          <React.Suspense fallback={<div />}>
+            <ClientSideOnlyLazy />
+          </React.Suspense>
+        )}
       </BaseLayout>
     );
   };
