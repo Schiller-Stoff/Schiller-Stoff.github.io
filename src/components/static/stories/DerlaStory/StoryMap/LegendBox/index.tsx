70e85f6a6e2cd9112a9c063cbfbcef18f50a8664
Sebi <SebastianStoff@gmx.at>
2022-02-23 17:51:11 +0100

    #40. added possibilty to expand legend box

--- src/components/static/stories/DerlaStory/StoryMap/LegendBox/index.tsx
+++ src/components/static/stories/DerlaStory/StoryMap/LegendBox/index.tsx
@@ -5,9 +5,13 @@ interface Props {
 }
 
 const LegendBox: React.FC<Props> = (props) => {
+
+  const [isExpanded, setIsExpanded] = React.useState<boolean>(false)
+
   return (
     <p
-      className="p-1 ps-3 pe-3 text-dark m-0"
+      onClick={() => setIsExpanded(!isExpanded)}
+      className="p-1 ps-3 pe-3 text-dark m-0 btn text-start"
       style={{
         position: "fixed",
         bottom: 0,
@@ -15,7 +19,8 @@ const LegendBox: React.FC<Props> = (props) => {
         right: "0%",
         background: "white",
         maxWidth: "20%",
-        border:"5px solid #789283"
+        border:"3px solid #789283",
+        borderBottom:"none"
         // opacity:.95
       }}
     >
@@ -24,13 +29,13 @@ const LegendBox: React.FC<Props> = (props) => {
           ? "Example places of remembrance"
           : "DERLA's area of research"}
       </strong>
-      <p className="text-dark">
+      {isExpanded && <p className="text-dark">
         <small>
           DERLA documents places of remembrances in the red areas. (Styria and
           Vorarlberg in today's Austria) Additionally this map shows the borders
           of the German Reich in 1941
         </small>
-      </p>
+      </p>}
     </p>
   );
 };
