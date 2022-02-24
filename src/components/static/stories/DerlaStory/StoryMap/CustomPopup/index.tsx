c456f3f5626912a089cf879a62eb40943f2ecaa7
Sebastian Stoff <sebastianstoff@gmx.at>
2022-02-24 17:42:37 +0100

    #40. added custom popup component

---
+++ src/components/static/stories/DerlaStory/StoryMap/CustomPopup/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Props {
+  label: string;
+  href: string;
+  imgSrc?: string;
+}
+
+const CustomPopup: React.FC<Props> = (props) => {
+  return (
+    <div style={{ filter: "grayscale(1)" }}>
+      <a href={props.href} target="_blank">
+        <p className="h5">{props.label}</p>
+        {props.imgSrc && (
+          <img src={props.imgSrc} width={200} height={200} loading="lazy"></img>
+        )}
+      </a>
+    </div>
+  );
+};
+
+export default CustomPopup;
