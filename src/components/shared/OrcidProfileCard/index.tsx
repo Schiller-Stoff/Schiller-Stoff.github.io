e69bd74a149fcbb1cd7dd825aeb4670f621ba423
Sebi <SebastianStoff@gmx.at>
2022-06-06 10:55:28 +0200

    #41. removed br

--- src/components/shared/OrcidProfileCard/index.tsx
+++ src/components/shared/OrcidProfileCard/index.tsx
@@ -12,7 +12,6 @@ const OrcidProfileCard: React.FC<Props> = ({ orcidRoot }) => {
     <>
       <ContentHeading>Interests and skills</ContentHeading>
       <br></br>
-      <br></br>
       <Row>
         <Col lg={6}>
           {/* <Button variant="secondary">Request meeting</Button> */}
@@ -29,10 +28,10 @@ const OrcidProfileCard: React.FC<Props> = ({ orcidRoot }) => {
           </div>
         </Col>
         <Col md={5} xl={6}>
-          <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>My primary background</Accordion.Header>
-              <Accordion.Body>
+          <Accordion defaultActiveKey="0" flush style={{background:"red"}}>
+            <Accordion.Item style={{background:"#233554"}} eventKey="0">
+              <Accordion.Header style={{background:"#233554"}}>My primary background</Accordion.Header>
+              <Accordion.Body style={{background:"#233554"}}>
                 {orcidRoot.person.keywords.keyword[0] && (
                   <>
                     <ul>
@@ -50,7 +49,7 @@ const OrcidProfileCard: React.FC<Props> = ({ orcidRoot }) => {
             </Accordion.Item>
             <Accordion.Item eventKey="1">
               <Accordion.Header>My Links</Accordion.Header>
-              <Accordion.Body>
+              <Accordion.Body style={{background:"#233554"}}>
                 {orcidRoot.person["researcher-urls"]["researcher-url"][0] && (
                   <>
                     <ul>
@@ -62,7 +61,6 @@ const OrcidProfileCard: React.FC<Props> = ({ orcidRoot }) => {
                             <a
                               rel="noopener"
                               target="_blank"
-                              className="text-dark"
                               href={rurl.url.value.toString()}
                             >
                               {rurl["url-name"] ? rurl["url-name"] : "Link"}
