b47c9b89db63845c670167e65da32c4669ea4c2d
Sebi <SebastianStoff@gmx.at>
2022-06-06 18:29:56 +0200

    #41. updated icon bar

--- src/components/shared/OrcidIconBar/index.tsx
+++ src/components/shared/OrcidIconBar/index.tsx
@@ -1,4 +1,4 @@
-import { faGithub, faMailchimp, faNpm, faOrcid, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faMailchimp, faNpm, faOrcid, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -21,24 +21,30 @@ const OrcidIconBar: React.FC<Props> = (props) => {
   const faIconStyle: React.CSSProperties = {
     height: 30,
     width: 30,
-    marginRight: ".5em",
+    marginRight: ".125em",
     display: "inline-block",
     color:"black",
-    background:"whitesmoke",
+    // background:"whitesmoke",
+  }
+
+  const directIconStyle: React.CSSProperties = {
+    fontSize: "1.125em"
   }
 
   return (
-    <div>
-      {props.mail && <a aria-label="Write me an e-mail" style={faIconStyle} href={`mailTo:${props.mail}`}><FontAwesomeIcon icon={faEnvelope} size="1x"></FontAwesomeIcon></a>}
+    <div className="mt-1">
+      {props.mail && <a aria-label="Write me an e-mail" style={faIconStyle} href={`mailTo:${props.mail}`}><FontAwesomeIcon icon={faEnvelope} style={directIconStyle}></FontAwesomeIcon></a>}
 
-      <a rel="noopener" style={faIconStyle} href={props.orcidURL} target="_blank" aria-label="See my ORCID record."><FontAwesomeIcon icon={faOrcid} size="1x"></FontAwesomeIcon></a>
+      <a rel="noopener" style={faIconStyle} href={props.orcidURL} target="_blank" aria-label="See my ORCID record."><FontAwesomeIcon icon={faOrcid} style={directIconStyle}></FontAwesomeIcon></a>
       {props.researchURLs.map((rurl, index) => {
         if (rurl.url.value.toString().includes("github")) {
-          return <a aria-label="See my code on github" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faGithub} size="1x"></FontAwesomeIcon></a>
+          return <a aria-label="See my code on github" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faGithub} style={directIconStyle}></FontAwesomeIcon></a>
         } else if (rurl.url.value.toString().includes("npmjs.com")) {
-          return <a aria-label="Find my javascript code on npmjs.com" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faNpm} size="1x"></FontAwesomeIcon></a>
+          return <a aria-label="Find my javascript code on npmjs.com" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faNpm} style={directIconStyle}></FontAwesomeIcon></a>
         } else if (rurl.url.value.toString().includes("twitter.com")) {
-          return <a aria-label="Connect with me on twitter.com" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faTwitter} size="1x"></FontAwesomeIcon></a>
+          return <a aria-label="Connect with me on twitter.com" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faTwitter} style={directIconStyle}></FontAwesomeIcon></a>
+        } else if (rurl.url.value.toString().includes("linkedin.com")) {
+          return <a aria-label="Connect with me on linkedin.com" rel="noopener" style={faIconStyle} key={index} href={rurl.url.value.toString()} target="_blank"><FontAwesomeIcon icon={faLinkedin} style={directIconStyle}></FontAwesomeIcon></a>
         }
       })}
     </div>)
