4bbf1ae20b02bdeb01af945b561867ce906c21fc
Sebi <SebastianStoff@gmx.at>
2022-06-07 13:39:13 +0200

    #41. added overview page over stories

--- src/pages/stories/index.tsx
+++ src/pages/stories/index.tsx
@@ -3,43 +3,49 @@ import BaseLayout from "../../components/shared/layouts/BaseLayout";
 import OrcidHead from "../../components/shared/OrcidHead";
 import OrcidNav from "../../components/shared/OrcidNav";
 import { Orcid } from "../../types/orcid";
-import ContentHeading from "../../components/shared/ContentHeading";
-
-/**
- * React scrollama needs to be lazy loaded. https://www.gatsbyjs.com/docs/using-client-side-only-packages/#workaround-3-load-client-side-dependent-components-with-loadable-components
- * Because of client only calculations that breaks SSR.
- */
-const ClientSideOnlyLazy = React.lazy(
-  () => import("../../components/static/stories/DerlaStory")
-);
+import { Link } from "gatsby";
+import { Card, Col, Row } from "react-bootstrap";
 
 const StoriesPage: React.FC<{ pageContext: { persOrcid: Orcid.RootObject } }> =
   (props) => {
-    const isSSR = typeof window === "undefined";
-
     return (
       <BaseLayout
         orcidRoot={props.pageContext.persOrcid}
         headComponent={<OrcidHead orcidRoot={props.pageContext.persOrcid} />}
         nav={<OrcidNav orcidRoot={props.pageContext.persOrcid} />}
       >
-        <h1>Derla</h1>
-        <h2
-          className="h5 mb-4"
-          style={{ fontWeight: 300, fontSize: "1.35em" }}
-        >
-          A research project at University of Graz  
+        <h1>Stories</h1>
+        <h2 className="h5 mb-4" style={{ fontWeight: 300, fontSize: "1.35em" }}>
+          Visual story ("scrolly-")telling about the projects I've finished. For on overview about all my works visit the <Link to="/portfolio">Portfolio</Link> page.    
         </h2>
-        <hr/>
-        <p className="w-75 mb-0">The <a href="https://gams.uni-graz.at/context:derla" target="blank">Digital Landscape of Remembrance Austria (DERLA)</a> is a scientific documentation and mediation project. It documents Austrian <strong>places and signs</strong> of remembrance for the victims as well as the places of terror of National Socialism.</p>
+        <hr />
+
+        <Row xs={1} md={2} lg={3} className="g-4">
+          <Col>
+            <Link className="text-decoration-none" to="/stories/derla">
+              <Card>
+                <Card.Body>
+                  <Card.Title as="h3" className="h6">
+                    DERLA
+                  </Card.Title>
+                  <Card.Text style={{fontSize:".85em"}}>
+                    A research project at the University of Graz (together with ZIM Graz | CJS - Center for Jewish Studies) 
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        </Row>
         <br></br>
-        <ContentHeading>Places of remembrance</ContentHeading>
-        <p className="w-75">"Places of remembrance," memorials and monuments are visible signs of historical awareness in everyday life</p>
-        {!isSSR && (
-          <React.Suspense fallback={<div />}>
-            <ClientSideOnlyLazy />
-          </React.Suspense>
-        )}
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </BaseLayout>
     );
   };
